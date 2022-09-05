64e68759837d87445f035266f28ce0ad02ed4632
kingBook <412126604@qq.com>
2022-09-05 22:26:39 +0800

    update 2022-09-05 22:26

--- Assets/Scripts/ShowFPS.cs
+++ Assets/Scripts/ShowFPS.cs
@@ -8,17 +8,12 @@ public class ShowFPS : MonoBehaviour {
 
     private TMP_Text m_text;
 
-
-	// Use this for initialization
 	void Start () {
         m_text = GetComponent<TMP_Text>();
 	}
 	
-	// Update is called once per frame
 	void Update () {
-        float fps = 1.0f / UnityEngine.Time.smoothDeltaTime;
-        //m_fps = string.Format("FPS: {0}", 1.0f / UnityEngine.Time.smoothDeltaTime);
-        //GUI.Label(m_uiPosition, "FPS: " + fps.ToString());
+        float fps = 1.0f / Time.smoothDeltaTime;
         m_text.text = "FPS: " + fps.ToString();
     }
 }

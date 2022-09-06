420e35050438f9946a53f6f4909f88c9cc4e564f
kingBook <412126604@qq.com>
2022-09-07 01:41:36 +0800

    update 2022-09-07 01:41

--- Assets/AniInstancing/Scripts/runtimeHelper.cs
+++ Assets/AniInstancing/Scripts/runtimeHelper.cs
@@ -2,36 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace AnimationInstancing
-{
-    public class RuntimeHelper
-    {
+namespace AnimationInstancing {
+
+    public class RuntimeHelper {
+
         // Merge all bones to a single array and merge all bind pose
-        public static Transform[] MergeBone(SkinnedMeshRenderer[] meshRender, List<Matrix4x4> bindPose)
-        {
+        public static Transform[] MergeBone(SkinnedMeshRenderer[] meshRender, List<Matrix4x4> bindPose) {
             UnityEngine.Profiling.Profiler.BeginSample("MergeBone()");
             List<Transform> listTransform = new List<Transform>(150);
-            for (int i = 0; i != meshRender.Length; ++i)
-            {
+            for (int i = 0; i != meshRender.Length; ++i) {
                 Transform[] bones = meshRender[i].bones;
                 Matrix4x4[] checkBindPose = meshRender[i].sharedMesh.bindposes;
-                for (int j = 0; j != bones.Length; ++j)
-                {
+                for (int j = 0; j != bones.Length; ++j) {
 #if UNITY_EDITOR
                     Debug.Assert(checkBindPose[j].determinant != 0, "The bind pose can't be 0 matrix.");
 #endif
                     // the bind pose is correct base on the skinnedMeshRenderer, so we need to replace it
                     int index = listTransform.FindIndex(q => q == bones[j]);
-                    if (index < 0)
-                    {
+                    if (index < 0) {
                         listTransform.Add(bones[j]);
-                        if (bindPose != null)
-                        {
+                        if (bindPose != null) {
                             bindPose.Add(checkBindPose[j]);
                         }
-                    }
-                    else
-                    {
+                    } else {
                         bindPose[index] = checkBindPose[j];
                     }
                 }
@@ -41,8 +34,7 @@ namespace AnimationInstancing
             return listTransform.ToArray();
         }
 
-        public static Quaternion QuaternionFromMatrix(Matrix4x4 mat)
-        {
+        public static Quaternion QuaternionFromMatrix(Matrix4x4 mat) {
             Vector3 forward;
             forward.x = mat.m02;
             forward.y = mat.m12;

7a57ec75caec1fe199ae8ff005b07210c0e6e49c
kingBook <412126604@qq.com>
2022-09-16 18:23:48 +0800

    update 2022-09-16 18:23

--- Assets/TestURP/GPUInstancing/Scripts/BoxInstancing.cs
+++ Assets/TestURP/GPUInstancing/Scripts/BoxInstancing.cs
@@ -2,31 +2,29 @@
 
 
 public class BoxInstancing : MonoBehaviour {
-    
+
     public int instanceCount = 10000;
     public Mesh instanceMesh;
     public Material instanceMaterial;
     public int subMeshIndex = 0;
-    
+
     private int cachedInstanceCount = -1;
     private int cachedSubMeshIndex = -1;
     private ComputeBuffer positionBuffer;
     private ComputeBuffer argsBuffer;
     private uint[] args = new uint[5] { 0, 0, 0, 0, 0 };
-    
+
     private void Start() {
         argsBuffer = new ComputeBuffer(1, args.Length * sizeof(uint), ComputeBufferType.IndirectArguments);
         UpdateBuffers();
     }
-    
+
     void Update() {
         // Update starting position buffer
-        if (cachedInstanceCount != instanceCount || cachedSubMeshIndex != subMeshIndex)
-            UpdateBuffers();
+        if (cachedInstanceCount != instanceCount || cachedSubMeshIndex != subMeshIndex) UpdateBuffers();
 
         // Pad input
-        if (Input.GetAxisRaw("Horizontal") != 0.0f)
-            instanceCount = (int)Mathf.Clamp(instanceCount + Input.GetAxis("Horizontal") * 40000, 1.0f, 5000000.0f);
+        if (Input.GetAxisRaw("Horizontal") != 0.0f) instanceCount = (int)Mathf.Clamp(instanceCount + Input.GetAxis("Horizontal") * 40000, 1.0f, 5000000.0f);
 
         // Render
         Graphics.DrawMeshInstancedIndirect(instanceMesh, subMeshIndex, instanceMaterial, new Bounds(Vector3.zero, new Vector3(100.0f, 100.0f, 100.0f)), argsBuffer);
@@ -38,13 +36,8 @@ public class BoxInstancing : MonoBehaviour {
     }
 
     void UpdateBuffers() {
-        // Ensure submesh index is in range
-        if (instanceMesh != null)
-            subMeshIndex = Mathf.Clamp(subMeshIndex, 0, instanceMesh.subMeshCount - 1);
-
         // Positions
-        if (positionBuffer != null)
-            positionBuffer.Release();
+        if (positionBuffer != null) positionBuffer.Release();
         positionBuffer = new ComputeBuffer(instanceCount, 16);
         Vector4[] positions = new Vector4[instanceCount];
         for (int i = 0; i < instanceCount; i++) {
@@ -63,9 +56,7 @@ public class BoxInstancing : MonoBehaviour {
             args[1] = (uint)instanceCount;
             args[2] = (uint)instanceMesh.GetIndexStart(subMeshIndex);
             args[3] = (uint)instanceMesh.GetBaseVertex(subMeshIndex);
-        }
-        else
-        {
+        } else {
             args[0] = args[1] = args[2] = args[3] = 0;
         }
         argsBuffer.SetData(args);
@@ -75,12 +66,10 @@ public class BoxInstancing : MonoBehaviour {
     }
 
     void OnDisable() {
-        if (positionBuffer != null)
-            positionBuffer.Release();
+        if (positionBuffer != null) positionBuffer.Release();
         positionBuffer = null;
 
-        if (argsBuffer != null)
-            argsBuffer.Release();
+        if (argsBuffer != null) argsBuffer.Release();
         argsBuffer = null;
     }
 }

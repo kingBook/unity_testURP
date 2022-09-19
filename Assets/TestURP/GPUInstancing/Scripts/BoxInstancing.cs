b021ecdc74aae91b601f24a73be153ea0f95c5ae
kingBook <412126604@qq.com>
2022-09-19 16:26:27 +0800

    update 2022-09-19 16:26

--- Assets/TestURP/GPUInstancing/Scripts/BoxInstancing.cs
+++ Assets/TestURP/GPUInstancing/Scripts/BoxInstancing.cs
@@ -3,73 +3,66 @@
 
 public class BoxInstancing : MonoBehaviour {
 
-    public int instanceCount = 10000;
+    public int instanceCount = 20;
     public Mesh instanceMesh;
     public Material instanceMaterial;
-    public int subMeshIndex = 0;
+    public int subMeshIndex;
 
-    private int cachedInstanceCount = -1;
-    private int cachedSubMeshIndex = -1;
-    private ComputeBuffer positionBuffer;
-    private ComputeBuffer argsBuffer;
-    private uint[] args = new uint[5] { 0, 0, 0, 0, 0 };
+    private ComputeBuffer m_argsBuffer;
+    private readonly uint[] m_args = new uint[5] { 0, 0, 0, 0, 0 };
+
+    private ComputeBuffer m_transformBuffer;
+    private ComputeBuffer m_colorBuffer;
+    private static readonly int s_transformBuffer = Shader.PropertyToID("transformBuffer");
+    private static readonly int s_colorBuffer = Shader.PropertyToID("colorBuffer");
 
     private void Start() {
-        argsBuffer = new ComputeBuffer(1, args.Length * sizeof(uint), ComputeBufferType.IndirectArguments);
+        m_argsBuffer = new ComputeBuffer(1, m_args.Length * sizeof(uint), ComputeBufferType.IndirectArguments);
         UpdateBuffers();
     }
 
     void Update() {
-        // Update starting position buffer
-        if (cachedInstanceCount != instanceCount || cachedSubMeshIndex != subMeshIndex) UpdateBuffers();
-
-        // Pad input
-        if (Input.GetAxisRaw("Horizontal") != 0.0f) instanceCount = (int)Mathf.Clamp(instanceCount + Input.GetAxis("Horizontal") * 40000, 1.0f, 5000000.0f);
-
-        // Render
-        Graphics.DrawMeshInstancedIndirect(instanceMesh, subMeshIndex, instanceMaterial, new Bounds(Vector3.zero, new Vector3(100.0f, 100.0f, 100.0f)), argsBuffer);
-    }
-
-    void OnGUI() {
-        GUI.Label(new Rect(265, 25, 200, 30), "Instance Count: " + instanceCount.ToString());
-        instanceCount = (int)GUI.HorizontalSlider(new Rect(25, 20, 200, 30), (float)instanceCount, 1.0f, 5000000.0f);
+        Graphics.DrawMeshInstancedIndirect(instanceMesh, subMeshIndex, instanceMaterial, new Bounds(Vector3.zero, new Vector3(100.0f, 100.0f, 100.0f)), m_argsBuffer);
     }
 
     void UpdateBuffers() {
-        // Positions
-        if (positionBuffer != null) positionBuffer.Release();
-        positionBuffer = new ComputeBuffer(instanceCount, 16);
-        Vector4[] positions = new Vector4[instanceCount];
+        // Positions and colors
+        m_transformBuffer?.Release();
+        m_transformBuffer = new ComputeBuffer(instanceCount, 64);
+        m_colorBuffer?.Release();
+        m_colorBuffer = new ComputeBuffer(instanceCount, 16);
+        Matrix4x4[] matrixs = new Matrix4x4[instanceCount];
+        Color[] colors = new Color[instanceCount];
         for (int i = 0; i < instanceCount; i++) {
-            float angle = Random.Range(0.0f, Mathf.PI * 2.0f);
-            float distance = Random.Range(20.0f, 100.0f);
-            float height = Random.Range(-2.0f, 2.0f);
-            float size = Random.Range(0.05f, 0.25f);
-            positions[i] = new Vector4(Mathf.Sin(angle) * distance, height, Mathf.Cos(angle) * distance, size);
+
+            matrixs[i] = Matrix4x4.TRS(new Vector3(Random.Range(-10, 10), 0f, Random.Range(-10, 10)), Random.rotation, Vector3.one);
+            colors[i] = Random.ColorHSV();
         }
-        positionBuffer.SetData(positions);
-        instanceMaterial.SetBuffer("positionBuffer", positionBuffer);
+        m_transformBuffer.SetData(matrixs);
+        m_colorBuffer.SetData(colors);
+        instanceMaterial.SetBuffer(s_transformBuffer, m_transformBuffer);
+        instanceMaterial.SetBuffer(s_colorBuffer, m_colorBuffer);
 
         // Indirect args
         if (instanceMesh != null) {
-            args[0] = (uint)instanceMesh.GetIndexCount(subMeshIndex);
-            args[1] = (uint)instanceCount;
-            args[2] = (uint)instanceMesh.GetIndexStart(subMeshIndex);
-            args[3] = (uint)instanceMesh.GetBaseVertex(subMeshIndex);
+            m_args[0] = (uint)instanceMesh.GetIndexCount(subMeshIndex);
+            m_args[1] = (uint)instanceCount;
+            m_args[2] = (uint)instanceMesh.GetIndexStart(subMeshIndex);
+            m_args[3] = (uint)instanceMesh.GetBaseVertex(subMeshIndex);
         } else {
-            args[0] = args[1] = args[2] = args[3] = 0;
+            m_args[0] = m_args[1] = m_args[2] = m_args[3] = 0;
         }
-        argsBuffer.SetData(args);
-
-        cachedInstanceCount = instanceCount;
-        cachedSubMeshIndex = subMeshIndex;
+        m_argsBuffer.SetData(m_args);
     }
 
-    void OnDisable() {
-        if (positionBuffer != null) positionBuffer.Release();
-        positionBuffer = null;
+    void OnDestroy() {
+        m_argsBuffer?.Release();
+        m_argsBuffer = null;
 
-        if (argsBuffer != null) argsBuffer.Release();
-        argsBuffer = null;
+        m_transformBuffer?.Release();
+        m_transformBuffer = null;
+
+        m_colorBuffer?.Release();
+        m_colorBuffer = null;
     }
 }
---
+++ Assets/TestURP/GPUInstancing/Shaders/LitBox.shader
@@ -0,0 +1,99 @@
+﻿Shader "TestURP/LitBox" {
+    Properties {
+        [MainTexture] _BaseMap("Albedo", 2D) = "white" {}
+        [MainColor] _BaseColor("Color", Color) = (1,1,1,1)
+    }
+    SubShader {
+        Tags {
+            "RenderType" = "Opaque" "RenderPipeline" = "UniversalPipeline" "IgnoreProjector" = "True"
+        }
+        LOD 300
+
+        Pass {
+            Name "ForwardLit"
+            Tags {
+                "LightMode" = "UniversalForward"
+            }
+
+            ZWrite On
+            ZTest On
+            Cull Off
+
+            HLSLPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Common.hlsl"
+            #include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Filtering.hlsl"
+            #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+            #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Input.hlsl"
+            #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
+
+            // Material Keywords
+            #pragma shader_feature _RECEIVE_SHADOWS_OFF
+
+            // Universal Pipeline keywords
+            #pragma multi_compile _ _MAIN_LIGHT_SHADOWS
+            #pragma multi_compile _ _MAIN_LIGHT_SHADOWS_CASCADE
+
+            // GPU Instancing
+            #pragma multi_compile_instancing
+
+            struct Attributes
+            {
+                float4 positionOS : POSITION;
+                uint instanceID : SV_InstanceID;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct Varyings
+            {
+                float4 positionHCS : SV_POSITION;
+                half4 color : COLOR;
+                float2 uv : TEXCOORD0;
+            };
+
+
+            TEXTURE2D_X(_BaseMap);
+            SAMPLER(sampler_BaseMap);
+
+            StructuredBuffer<float4x4> transformBuffer;
+            StructuredBuffer<half4> colorBuffer;
+
+
+            Varyings vert(Attributes input)
+            {
+                float4 positionOS = input.positionOS;
+                uint instanceID = input.instanceID;
+
+                // transform
+                float4x4 m = transformBuffer[instanceID];
+                positionOS = mul(m, positionOS);
+
+                Varyings output;
+                output.positionHCS = mul(UNITY_MATRIX_VP, positionOS);
+                output.color = colorBuffer[instanceID];
+                return output;
+            }
+
+            half4 frag(Varyings input): SV_Target
+            {
+                half4 diffuseColor = SAMPLE_TEXTURE2D_X(_BaseMap, sampler_BaseMap, input.uv);
+
+                //计算光照和阴影，光照采用Lembert Diffuse.
+                float4 shadowCoord = TransformWorldToShadowCoord(input.positionWS);
+                Light mainLight = GetMainLight(shadowCoord);
+                float3 lightDir = mainLight.direction;
+                float3 lightColor = mainLight.color;
+                float3 normalWS = input.normalWS;
+                float4 color = float4(1, 1, 1, 1);
+                float minDotLN = 0.2;
+                color.rgb = max(minDotLN, abs(dot(lightDir, normalWS))) * lightColor * diffuseColor.rgb * _BaseColor.rgb
+                    * mainLight.shadowAttenuation;
+                return color;
+            }
+            ENDHLSL
+        }
+    }
+    FallBack "Hidden/Universal Render Pipeline/FallbackError"
+}
